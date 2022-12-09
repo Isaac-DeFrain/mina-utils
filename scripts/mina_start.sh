53b231e3dc3895ebf905629d362d6161e6500b43
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-12-09 17:07:13 -0500

    Add mina daemon peer list url to mina_start

--- scripts/mina_start.sh
+++ scripts/mina_start.sh
@@ -2,3 +2,4 @@ systemctl --user daemon-reload
 systemctl --user start mina
 systemctl --user enable mina
 sudo loginctl enable-linger
+mina daemon --peer-list-url https://storage.googleapis.com/mina-seed-lists/mainnet_seeds.txt
