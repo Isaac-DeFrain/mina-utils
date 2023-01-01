970b1e94ae83a85dde8def4a7f33cd4592fd79d9
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-12-31 22:34:42 -0500

    Update voting constants

--- scripts/voting_stats_constants.py
+++ scripts/voting_stats_constants.py
@@ -1,19 +1,16 @@
-import  os
 import pathlib
 
 # voting
-START_TIME = "2022-12-16T15:00:00Z"
-END_TIME = "2022-12-31T11:59:59Z"
+START_TIME = "2022-12-16T10:00:00Z" # UTC-5:00
+END_TIME = "2022-12-31T06:59:59Z"   # UTC-5:00
 
-# local
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'voting_service_key.json'
 PARENT_DIR = pathlib.Path(__file__).parent
-
 LOCAL_DATA_DIR = PARENT_DIR / "voting-data"
 '''
 ./voting_data
 '''
 
-# mina explorer
-
-DATA_ARCHIVE = pathlib.Path('https://storage.googleapis.com/mina-explorer-ledgers/')
+MINA_EXPLORER = "https://graphql.minaexplorer.com"
+'''
+Mina Explorer GraphQL endpoint
+'''
